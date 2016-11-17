c61d55f96b0d1b690a7b9449a863e5eaf84df069
Dave O'Dea <daveodea@gmail.com>
2016-11-17 11:17:58 +0000

    adding JSON schemas

---
+++ .docs/JSONSchemas.js
@@ -0,0 +1,27 @@
+{ //movie db
+    "title" : "movie1",
+    "genre" : "thriller",
+    "length": "feature",
+    "shortDescript": "bla bla",
+    "addComm" : "bla bla bla",
+    "storageLink" : "http..." //place upload button on submission UI
+}
+
+{ //screening cal
+    "shortDescript": "bla bla",
+    "date" : "dd/mm/yy",
+    "time" : "00:00",
+    "venue" : "place"
+}
+
+{//user creation    
+    "name" : "david",
+    "email" : "test@test.com",
+    "contactNo" : "12345678",
+    "userType"{
+        "teacher" : "bool",
+        "student" : "bool",
+        "other" : "info",
+    }
+    "terms" : "bool"
+}
