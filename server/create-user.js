826f2d127f19f9d3171d2764cb153e7e0a2fcb09
James <j.quaife1@nuigalway.ie>
2017-02-22 16:18:29 +0000

    add server/create-user.js client/templates/signup.js

--- server/create-user.js
+++ server/create-user.js
@@ -1,11 +1,12 @@
 Accounts.onCreateUser( function( options, user ) {
   if ( options.profile && options.profile.user ) {
-     var optionsUser        = options.profile.user;
-
+  
+    var optionsUser  = options.profile.user;
+    
+  
+     
     optionsUser.userId = user._id;
-    //delete options.profile;
-
-    //Users.insert( optionsUser );
+   
     user.profile = options.profile;
   }
 
@@ -14,4 +15,6 @@ Accounts.onCreateUser( function( options, user ) {
   }
 
   return user;
+  
+  
 });
