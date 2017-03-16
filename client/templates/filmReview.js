8523d3316969e35a89d3900ed75c2d815cc33552
Andrew Reid East <andrew@andreweast.net>
2017-03-16 11:23:01 +0000

    User video page displays everything except video itself

--- client/templates/filmReview.js
+++ client/templates/filmReview.js
@@ -1,31 +1,40 @@
+// https://kadira.io/academy/meteor-routing-guide/content/subscriptions-and-data-management/with-blaze
+Template.filmReview.onCreated(function() {
+    let self = this;
+    self.autorun(function() {
+        let usr = FlowRouter.getParam('userId');
+        // subscribe to the published user data (defined in users.js):
+        self.subscribe('users.one', usr);
+
+        console.log("INSIDE AUTORUN");
+        console.log('usr=' + usr);
+        // let userVideo = Users.find({
+        //     _id: FlowRouter.getParam('userId')
+        // });
+        // console.log('found:')
+        // console.log(userVideo);
+        // console.log('userVideo.submittedFilm=')
+        // console.log(userVideo.submittedFilm);
+        // let video = Films.collection.findOne({ _id: userVideo.submittedFilm._fileId});
+        // console.log('found:');
+        // console.log(video);
+    });
+});
+
 Template.filmReview.helpers({
-    options: function() {
-        return {
-            defaultView: 'month',
-            firstDay: 1,
-            defaultDate: '2017-5-1',
-            header: {
-                left: '',
-                center: '',
-                right: 'basicWeek month'
-            },
-            footer: {
-                left: '',
-                center: '',
-                right: ''
-            },
-            events: [
-                {
-                    title: 'Film Showing',
-                    start: '2017-5-1T10:00:00',
-                    end: '2017-5-1-T12:00:00'
-                    // rendering: 'background'
-                }
-            ],
-            dayClick: function(date, jsEvent, view) {
-                alert('clicked on: ' + date.format());
-                $(this).css('background-color', 'teal');
-            }
-        }
+    userData: function() {
+        // get data:
+        // note: despite using "findOne()", this does depend on the Subscription to get the proper user data
+        let usrVideo = Users.findOne({
+            _id: FlowRouter.getParam('userId')
+        }) || {};
+        // note: .count() won't work to see if one was retrieved or not: usrVideo.count());
+        // TODO: error handling: how to show if attemping to show a video that's not found
+        // TODO: show route with slug rather than _id. idea: https://github.com/deborah-ufw/flow-router-dynamic-links-use-slug
+        console.log(usrVideo);
+        return usrVideo;
+    },
+    video: function() {
+        // var
     }
 });
