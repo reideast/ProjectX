4ac14e046f96576a88b9fb929eea89600fc95762
Andrew Reid East <andrew@andreweast.net>
2017-03-26 15:22:54 +0100

    Clean up viewFilms js, put film thumbnails into columns

--- client/templates/viewFilms.js
+++ client/templates/viewFilms.js
@@ -8,26 +8,11 @@ Template.viewFilms.onRendered(function() {
 
 Template.viewFilms.helpers({
     filmListing: Users.find({ submittedFilm: { $exists: true } }),
-    
-    thumbRef: function() {
-        // TODO: this is currently broken. after a "meteor reset" and creating a user, it showed a that user's film
-        // TODO: the problem is that you're searching "this", which gives the logged in user!!
-        // if (argId) {
-        console.log("inside the thumbURL template helper");
-        console.log("this is");
-        console.log(this);
 
+    thumbRef: function() {
         if (this.submittedFilm) {
             let film = Films.collection.findOne({ _id: this.submittedFilm.fileId});
-            // let film = Films.findOne({_id: argId});
-            console.log("done searching the db. film=");
-            console.log(film);
-
-            // film.versions.thumbnail
             return film;
-        } else {
-            console.log("ERR: No submitted film for THIS");
         }
-        // }
     }
 });
