73a7f3eb4c4f47a1a92b8aceeb6083c8e2e9e114
Andrew Reid East <andrew@andreweast.net>
2017-03-29 17:18:58 +0100

    Working email system

--- client/templates/filmSubmission.js
+++ client/templates/filmSubmission.js
@@ -144,5 +144,15 @@ Template.filmSubmission.events({
     'change #filmTermsAccepted': function(e, template) {
         template.hasAcceptedTerms = e.currentTarget.checked;
         template.testEnableSubmit();
+    },
+    'click .resend-verification-link' ( event, template ) {
+        Meteor.call( 'sendVerificationLink', ( error, response ) => {
+            if ( error ) {
+                Bert.alert( error.reason, 'danger' );
+            } else {
+                let email = Meteor.user().emails[ 0 ].address;
+                Bert.alert( `Verification sent to ${ email }!`, 'success', 'growl-top-right' );
+            }
+        });
     }
 });
