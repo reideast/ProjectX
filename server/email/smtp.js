c440508f77f5525a8adb28362dafb5ea8dbec1e4
Andrew Reid East <andrew@andreweast.net>
2017-04-01 19:16:07 +0100

    Test environmental variables

--- server/email/smtp.js
+++ server/email/smtp.js
@@ -1,3 +1,27 @@
 Meteor.startup(function() {
-    process.env.MAIL_URL = 'smtp://davidodea:Test1234*@smtp.sendgrid.net:587';
+    if (process.env.MAIL_URL) {
+        console.log("Found a MAIL_URL!");
+        console.log(process.env.MAIL_URL);
+    } else {
+        console.log("ERROR: Did not find a MAIL_URL");
+    }
+
+    if (process.env.DROPBOX) {
+        console.log("Found a DROPBOX!");
+        console.log(process.env.DROPBOX);
+        const dropbox = JSON.parse(process.env.DROPBOX).dropbox;
+        console.log("JSON:");
+        console.log(dropbox);
+        console.log("meteor.settings:");
+        console.log(Meteor.settings);
+        Meteor.settings.dropbox = JSON.parse(process.env.DROPBOX).dropbox;
+        console.log("Added dropbox:");
+        console.log(Meteor.settings);
+    } else {
+        console.log("ERROR: Did not find a MAIL_URL");
+    }
+
+
+    var dropbox = Npm.require('dropbox');
+    console.log(dropbox);
 });
